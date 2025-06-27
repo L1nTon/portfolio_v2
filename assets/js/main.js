dac7b9c833f7ea6d4d0a978890eb2a56b1faff17
charmain <charmain@student.21-school.ru>
2025-06-27 19:54:28 +0500

    first

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const header = document.querySelector("header");
+
+
+window.onscroll = () => {
+    if(window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+}
+document.querySelector('.page-1').addEventListener("mousemove", function(e){
+    let imgs = document.querySelectorAll(".img-content img");
+    let valX = e.clientX;
+    let valY = e.clientY-100;
+
+    imgs.forEach((img, i) => {
+        let speed = (i + 1) * 0.05;
+        let x = (window.innerWidth / 2 - valX) * speed;
+        let y = (window.innerHeight / 2 - valY) * speed;
+
+        img.style.transform = `translate(${x}px, ${y}px)`;
+    });
+});
+document.querySelector('.page-1').addEventListener("mouseleave", function(e){
+    let imgs = document.querySelectorAll(".img-content img");
+    imgs.forEach(img => {
+        img.style.transform = `translate(0px, 0px)`;
+    });
+});
+
